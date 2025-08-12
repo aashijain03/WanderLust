ba788f91a4f69b03831f6d13f362cda6ffc8178f
Aashi Jain <aashijain043@gmail.com>
2025-08-12 17:38:35 +0530

    added express routes for restructuring

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema,reviewSchema} = require("./schema.js");
-const Review = require("./models/review.js");
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 const MongoURL = "mongodb://127.0.0.1:27017/wanderLust";
 
@@ -19,7 +18,7 @@ main().then(()=>{
 })
 async function main(){
     await mongoose.connect(MongoURL);
-}
+};
 
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
@@ -32,104 +31,8 @@ app.get("/",(req,res)=>{
     res.send("hi i am root");
 });
 
-const validateListing = (req,res,next)=>{
-    let {err} = listingSchema.validate(req.body);
-    if(err){
-        let errMsg = err.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,err);
-    }else{
-        next();
-    }
-}
-const validateReview = (req,res,next)=>{
-    let {err} = reviewSchema.validate(req.body);
-    if(err){
-        let errMsg = err.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,err);
-    }else{
-        next();
-    }
-}
-
-//index route
-app.get("/listings",wrapAsync(async (req,res)=>{
-    const allListings = await Listing.find({});
-    res.render("listings/index",{allListings});
-}));
-
-//new route
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs");
-});
-
-//show route
-app.get("/listings/:id",wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show",{listing});
-}));
-
-//create route
-app.post("/listings",validateListing, wrapAsync(async(req,res)=>{
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-}));
-
-//Edit route
-app.get("/listings/:id/edit",wrapAsync( async(req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit",{listing});
-}));
-
-//update route
-app.put("/listings/:id",validateListing, wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}));
-
-//delete route
-app.delete("/listings/:id",wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
-
-//review route
-app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req,res)=>{
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listings/${listing._id}`);
-}));
-
-//Delete route
-app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
-    let{id,reviewId} = req.params;
-    await Listing.findByIdAndUpdate(id,{$pull:{reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/listings/${id}`);
-}));
-
-// app.get("/testListing",async (req,res)=>{
-//     let sampleListing = new Listing({
-//         title : "my new villa",
-//         description : "by the beach",
-//         price : 4000,
-//         location : "Goa",
-//         country : "India"
-//     });
-//     await sampleListing.save();
-//     console.log("sample was saved");
-//     res.send("successful testing");
-// });
+app.use("/listings",listings);
+app.use("/listings/:id/reviews",reviews);
 
 // app.all("*",(req,res,next)=>{
 //  next(new ExpressError(404,"Page not found!"));
